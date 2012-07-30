023f9036b4a20dbb27bd659d8cf0a6d193face25
Blue Swirl <blauwirbel@gmail.com>
2012-07-30 15:29:11 +0000

    sparc: fix expression with uninitialized initial value
    err was uninitialized, it's not OK to use |=. Spotted by Clang
    compiler.
    
    Fix by implementing the earlier statement which initializes the variable.
    
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- linux-user/signal.c
+++ linux-user/signal.c
@@ -1837,7 +1837,7 @@ typedef struct {
 } __siginfo_t;
 
 typedef struct {
-        unsigned   long si_float_regs [32];
+        abi_ulong       si_float_regs[32];
         unsigned   long si_fsr;
         unsigned   long si_fpqdepth;
         struct {
@@ -2048,11 +2048,9 @@ restore_fpu_state(CPUState *env, qemu_siginfo_fpu_t *fpu)
                 return -EFAULT;
 #endif
 
-#if 0
         /* XXX: incorrect */
-        err = __copy_from_user(&env->fpr[0], &fpu->si_float_regs[0],
-	                             (sizeof(unsigned long) * 32));
-#endif
+        err = copy_from_user(&env->fpr[0], fpu->si_float_regs[0],
+                             (sizeof(abi_ulong) * 32));
         err |= __get_user(env->fsr, &fpu->si_fsr);
 #if 0
         err |= __get_user(current->thread.fpqdepth, &fpu->si_fpqdepth);
